5b39c2166003ee4d5c0a7dc22b7ac097ecce5949
natanael tegar <natanael.tegar@ti.ukdw.ac.id>
2021-11-11 19:39:02 +0700

    Mencatat tambahan belanjaan

---
+++ 3_D_71210760.py
@@ -0,0 +1,7 @@
+daf1 = input("Masukkan daftar belanja 1: ")
+daf2 = input("Masukkan daftar belanja 2: ")
+tbhdaf1 = input("Tambahkan data ke daftar belanja 1: ")
+tbhdaf2 = input("Tambahkan data ke daftar belanja 2: ")
+
+print("Daftar belanja 1 adalah ", daf1, ", ", tbhdaf1)
+print("Daftar belanja 2 adalah ", daf2, ", ", tbhdaf2)
