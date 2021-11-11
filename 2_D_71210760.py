cc3c699bbf3013ea64e9b9af963d8c88dadea4c4
natanael tegar <natanael.tegar@ti.ukdw.ac.id>
2021-11-11 19:18:10 +0700

    Menghitung rata-rata uang saku

---
+++ 2_D_71210760.py
@@ -0,0 +1,5 @@
+saku1 = int(input("Uang saku anak pertama : "))
+saku2 = int(input("Uang saku anak kedua : "))
+saku3 = int(input("Uang saku anak ketiga : "))
+avg = (saku1+saku2+saku3)/3
+print("Rata-rata dari ", saku1, "+", saku2, "+", saku3, "adalah ", avg)
